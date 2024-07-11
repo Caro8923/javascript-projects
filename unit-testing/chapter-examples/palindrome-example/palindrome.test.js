e4dde7db8be221179de2b47e60dd0b4958005215
Carolyn Smith <carolynsmith@slu.edu>
2024-07-11 18:04:24 -0500

    Palindrome examples

---
+++ unit-testing/chapter-examples/palindrome-example/palindrome.test.js
@@ -0,0 +1,25 @@
+const isPalindrome = require('./palindrome.js');
+
+describe("testing isPalindrome", function(){
+   
+    test("should return true for a single letter", function() {
+        expect(isPalindrome("a")).toBe(true);
+    });
+
+    test("should return true for a single letter repeated", function() {
+        expect(isPalindrome("aaa")).toBe(true);
+    });
+
+    test("should return true for a simple palindrome", function() {
+        expect(isPalindrome("aba")).toBe(true);
+    });
+
+    test("should return true for a longer palindrome", function() {
+        expect(isPalindrome("racecar")).toBe(true);
+    });
+
+    test("should consider the empty string a palindrome", function() {
+        expect(isPalindrome("")).toBe(true);
+    });
+
+});
