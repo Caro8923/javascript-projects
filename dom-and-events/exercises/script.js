b56ba3f4696364d823b4169a113a3a120f80016b
Carolyn Smith <carolynsmith@slu.edu>
2024-08-03 21:59:17 -0500

    Exercises minus the last task

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -3,8 +3,22 @@ function init () {
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
+    button.addEventListener('click', event=> {
+        paragraph.innerHTML = 'Houston! We have liftoff!';
+    });
+
+    missionAbort.addEventListener('mouseover', function(event) {
+        event.target.style.backgroundColor= "red";
+    })
     
+    missionAbort.addEventListener('mouseout', function(event) {
+        event.target.style.backgroundColor="";
+    })
+
+    missionAbort.addEventListener('click', event => {
+        //input from readline sync?
+        //if YES the print message
+    })
 }
 
 window.addEventListener("load", init);
